69e3fc6ecd41d0a68cc84d77a4a9f0150c5664e6
Michelle Callista <61784086+MCallista@users.noreply.github.com>
2021-03-29 20:16:28 +0700

    Update bikeShareComp.R

--- bikeShareComp.R
+++ bikeShareComp.R
@@ -1,15 +1,21 @@
 library(dplyr)
 
-#weather
-wthrCnt <- londonBike %>% group_by(weather_code)
-temp <- wthrCnt %>% summarise(cnt = mean(cnt))
-barplot(height=temp$cnt, names=temp$weather_code, col=rgb(0.2,0.4,0.6,0.6), xlab="Weather Code", ylab="Average Bike share", main="Bike share according to weather")
-
 #season
-barplot(height = temp2$cnt, names = temp2$season, xlab = "season code", ylab = "Average Bike Share", main = "Bike share according to season")
+seasonCnt <- (londonBike %>% group_by(season))%>%summarise(cnt = mean(cnt))
+barplot(height = seasonCnt$cnt, names = seasonCnt$season, col=rgb(0.9,0.3,0.2),
+        xlab = "season code", ylab = "Average Bike Share", main = "Bike share according to season")
 
 #weekend
-barplot(height = temp3$cnt, names=temp3$is_weekend, xlab = "weekend code" , ylab = "Average Bike Share", main = "Bike share according to weekend")
+weekendCnt <- (londonBike %>% group_by(is_weekend))%>%summarise(cnt = mean(cnt))
+barplot(height = weekendCnt$cnt, names=weekendCnt$is_weekend, col=rgb(1,1,0.5),
+        xlab = "weekend code" , ylab = "Average Bike Share", main = "Bike share according to weekend")
 
 #holiday
-barplot(height = temp4$cnt, names = temp4$is_holiday, xlab = "holiday code", ylab = "Average Bike Share", main = "Bike share according to holiday")
+holidayCnt <- (londonBike %>% group_by(is_holiday))%>%summarise(cnt = mean(cnt))
+barplot(height = holidayCnt$cnt, names = holidayCnt$is_holiday, col=rgb(0.5,1,0.3),
+        xlab = "holiday code", ylab = "Average Bike Share", main = "Bike share according to holiday")
+
+#weather
+weatherCnt <- (londonBike %>% group_by(weather_code))%>%summarise(cnt = mean(cnt))
+barplot(height=weatherCnt$cnt, names=weatherCnt$weather_code, col=rgb(0,0.6,0.8),
+        xlab = "Weather Code", ylab = "Average Bike share", main= "Bike share according to weather")
