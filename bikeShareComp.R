c3a975261cffd8a706689b45e7684d1983b0efda
Peter Iskandar <68910543+Erkapos@users.noreply.github.com>
2021-04-13 18:57:51 +0700

    Update bikeShareComp.R

--- bikeShareComp.R
+++ bikeShareComp.R
@@ -22,4 +22,10 @@ barplot(height=weatherCnt$cnt, names=weatherCnt$weather_code, col=rgb(0,0.6,0.8)
 
 #t1
 t1Cnt <- (londonBike %>% group_by(t1))%>%summarise(cnt = mean(cnt))
-plot(t1Cnt$t1, t1Cnt$cnt, type = "l", xlab = "t1(Real Temperature)", ylab = "cnt", main = "Bike share according to t1", col = "blue")
+plot(t1Cnt$t1, t1Cnt$cnt, type = "l", 
+        xlab = "t1(Real Temperature)", ylab = "cnt", main = "Bike share according to t1", col = "blue")
+
+#t2
+t2Cnt <- (londonBike %>% group_by(t2))%>%summarise(cnt = mean(cnt))
+plot(t2Cnt$t2, t2Cnt$cnt, type = "l", 
+     xlab = "t2(Temperature Feels Like)", ylab = "cnt", main = "Bike share according to t2", col = "blue")
