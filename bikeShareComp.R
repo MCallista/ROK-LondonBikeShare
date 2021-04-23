996287217afc02121fc7c0099d7a8cd5b7d673fd
Michelle Callista <61784086+MCallista@users.noreply.github.com>
2021-04-23 21:12:30 +0700

    Update bikeShareComp.R

--- bikeShareComp.R
+++ bikeShareComp.R
@@ -48,26 +48,49 @@ londonBike2 <- separate(londonBike, timestamp, c("date", "time"), sep = " ", dro
 londonBike2 <- separate(londonBike2, date, c("year", "month", "date"), sep = "-")
 londonBike2 <- separate(londonBike2, time, c("hour"), sep = ":")
 
+#Add day of week
+date1=substr(londonBike2$timestamp,1,10)
+days <- weekdays(as.Date(date1))
+londonBike2$days = days
+
+#BAR PLOT AJA YANG DIPAKE
+
 #date
 boxplot(cnt~date,data=londonBike2, main="Bike Share to Date", 
         xlab="Date", ylab="Bike Share count", col=rainbow(31, alpha=0.2))
 
-#day of week
-date = substr(londonBike2$timestamp,1,10)
-days <- weekdays(as.Date(date))
-londonBike2$timestamp = days
+dateCnt <- (londonBike2 %>% group_by(date))%>%summarise(cnt = mean(cnt))
+barplot(height=dateCnt$cnt, names=dateCnt$days, col=rainbow(31, alpha=0.2), 
+        xlab="Date", ylab="Average Bike share", main="Bike share according to date")
 
+#day of week
 boxplot(cnt~days,data=londonBike2, main="Bike Share to Days",
         xlab="Day", ylab="Bike Share count", col=rainbow(7, alpha=0.2))
+
+daysCnt <- (londonBike2 %>% group_by(days))%>%summarise(cnt = mean(cnt))
+barplot(height=daysCnt$cnt, names=daysCnt$days, col=rainbow(7, alpha=0.2), 
+        xlab="Days", ylab="Average Bike share", main="Bike share according to days")
 
 #month
 boxplot(cnt~month,data=londonBike2, main="Bike Share to Month", 
         xlab="Month", ylab="Bike Share count", col=rainbow(12, alpha=0.2))
 
+monthCnt <- (londonBike2 %>% group_by(month))%>%summarise(cnt = mean(cnt))
+barplot(height=monthCnt$cnt, names=monthCnt$month, col=rainbow(12, alpha=0.2), 
+        xlab="Month", ylab="Average Bike share", main="Bike share according to month")
+
 #year
 boxplot(cnt~year,data=londonBike2, main="Bike Share to Year", 
         xlab="Year", ylab="Bike Share count", col=rainbow(3, alpha=0.2))
 
+yearCnt <- (londonBike2 %>% group_by(year))%>%summarise(cnt = mean(cnt))
+barplot(height=yearCnt$cnt, names=yearCnt$year, col=rainbow(3, alpha=0.2), 
+        xlab="Year", ylab="Average Bike share", main="Bike share according to year")
+
 #hour
 boxplot(cnt~hour,data=londonBike2, main="Bike Share to Hour", 
         xlab="Hour", ylab="Bike Share count", col=rainbow(24, alpha=0.2))
+
+hourCnt <- (londonBike2 %>% group_by(hour))%>%summarise(cnt = mean(cnt))
+barplot(height=hourCnt$cnt, names=hourCnt$days, col=rainbow(24, alpha=0.2), 
+        xlab="Hour", ylab="Average Bike share", main="Bike share according to hour")
