6d5c869297745a64ebcc64bf10086167e4c3c4a5
M Marzuqi Laksamana <49791562+pyrohunter13@users.noreply.github.com>
2021-04-24 22:32:48 +0700

    Update app.R

--- app.R
+++ app.R
@@ -14,6 +14,12 @@ t1 <- (londonBike %>% group_by(t1))%>%summarise(cnt = mean(cnt))
 t2 <- (londonBike %>% group_by(t2))%>%summarise(cnt = mean(cnt))
 hum <- (londonBike %>% group_by(hum))%>%summarise(cnt = mean(cnt))
 wind <- (londonBike %>% group_by(wind_speed))%>%summarise(cnt = mean(cnt))
+londonBike2 <- separate(londonBike, timestamp, c("date", "time"), sep = " ", drop(FALSE))
+londonBike2 <- separate(londonBike2, date, c("year", "month", "date"), sep = "-")
+londonBike2 <- separate(londonBike2, time, c("hour"), sep = ":")
+date = substr(londonBike2$timestamp,1,10)
+days <- weekdays(as.Date(date))
+londonBike2$timestamp = days
 
 #R Shiny ui
 ui <- dashboardPage(
@@ -31,7 +37,7 @@ ui <- dashboardPage(
       tabItem('plots', 
               #Histogram filter
               box(status = 'primary', title = 'eda', 
-                  selectInput('val', 'Variables:', c('Season', 'Weekend', 'Holiday', 'Weather','T1', 'T2', 'Humidity', 'Wind Speed')),
+                  selectInput('val', 'Variables:', c('Season', 'Weekend', 'Holiday', 'Weather','T1', 'T2', 'Humidity', 'Wind Speed', 'Date', 'Date of Week', 'Month', 'Year', 'Hour')),
                   
                   footer = ''),
               #Boxes to display the plots
@@ -47,7 +53,12 @@ server <- function(input, output) {
                                                  ifelse(input$val == 'T1', 't1',
                                                     ifelse(input$val == 'T2', 't2',
                                                          ifelse(input$val == 'Humidity', 'hm',
-                                                                ifelse(input$val == 'Wind Speed', 'ws'))))))))
+                                                                ifelse(input$val == 'Wind Speed', 'ws',
+                                                                      ifelse(input$val == 'Date', 'dt',
+                                                                            ifelse(input$val == 'Date of Week', 'dw',
+                                                                                  ifelse(input$val == 'Month', 'mn',
+                                                                                        ifelse(input$val == 'Year', 'yr',
+                                                                                              ifelse(input$val == 'Hour', 'hr')))))))))))))
     if(val=='ssn'){
       barplot(height = season$cnt, names = season$season, col=rgb(0.9,0.3,0.2),
               xlab = "season code", ylab = "Average Bike Share", main = "Bike share according to season")
@@ -79,6 +90,26 @@ server <- function(input, output) {
     if(val == 'ws'){
       plot(wind$wind_speed, wind$cnt, type = "l", 
            xlab = "wind speed", ylab = "cnt", main = "Bike share according to wind speed", col = "blue")
+    }
+    if(val == 'dt'){
+      boxplot(cnt~date,data=londonBike2, main="Bike Share to Date", 
+              xlab="Date", ylab="Bike Share count", col=rainbow(31, alpha=0.2))
+    }
+    if(val == 'dw'){
+      boxplot(cnt~days,data=londonBike2, main="Bike Share to Days",
+              xlab="Day", ylab="Bike Share count", col=rainbow(7, alpha=0.2))
+    }
+    if(val == 'mn'){
+      boxplot(cnt~month,data=londonBike2, main="Bike Share to Month", 
+              xlab="Month", ylab="Bike Share count", col=rainbow(12, alpha=0.2))
+    }
+    if(val == 'yr'){
+      boxplot(cnt~year,data=londonBike2, main="Bike Share to Year", 
+              xlab="Year", ylab="Bike Share count", col=rainbow(3, alpha=0.2))
+    }
+    if(val == 'hr'){
+      boxplot(cnt~hour,data=londonBike2, main="Bike Share to Hour", 
+              xlab="Hour", ylab="Bike Share count", col=rainbow(24, alpha=0.2))
     }
   })
 }
